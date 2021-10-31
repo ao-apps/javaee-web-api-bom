19d0cac00edeb6a2ce4b9cd632d5597e8d8ad400
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:54:47 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- book/src/main/java/module-info.java
+++ book/src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with javaee-web-api-bom.  If not, see <http://www.gnu.org/licenses/>.
+ * along with javaee-web-api-bom.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.javaee.webapi.bom.book {
 	// Direct
